db0cf9e63fd8f37c5e52fa825e3c5e5add780a66
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 02:19:38 +0000

    Automatically backed up by Learn

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,15 +1,6 @@
 # Enter your procedural solution here!
 require 'prime'
-  
-def find_all_primes(factors) 
-  factors.select {|number| 
-    Prime.prime?(number)}
-end
 
-def find_all_factors(number) 
-  (2...number).to_a.select {|factor| 
-    number % factor == 0}
-end 
 
 def largest_prime_factor(number) 
     number.prime_division.map(&:first).last
