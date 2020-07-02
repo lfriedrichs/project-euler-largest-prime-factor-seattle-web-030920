77dc36730fb8533f3c1c933edaa6b8c3c8776d5f
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 02:09:50 +0000

    Automatically backed up by Learn

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,10 +1,5 @@
 # Enter your procedural solution here!
-def is_a_prime(number)
-  (2...number).to_a.select {|div| 
-    if number % div == 0 
-      return false 
-    end}
-end
+require 'prime'
   
 def find_all_primes(limit) 
   (2...limit).to_a.select {|number| 
