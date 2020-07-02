1bb7a8bb49f301df8a4133317f506c9182351253
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 02:21:56 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,7 @@
 # Enter your procedural solution here!
+require 'prime'
+
+
+def largest_prime_factor(number) 
+    number.prime_division.map(&:first).last
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,11 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor 
+  def initialize(number) 
+    @number = number
+  end 
+  
+  def number
+    @number.prime_division.map(&:first).last
+  end
+  
+end 
