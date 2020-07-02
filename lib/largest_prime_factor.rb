4945b0cc81db85f0c79cd5aab354f1dcf8cc1f02
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 02:08:25 +0000

    Automatically backed up by Learn

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,4 +1,16 @@
 # Enter your procedural solution here!
+def is_a_prime(number)
+  (2...number).to_a.select {|div| 
+    if number % div == 0 
+      return false 
+    end}
+end
+  
+def find_all_primes(limit) 
+  (2...limit).to_a.select {|number| 
+    is_a_prime(number)}
+end
+
 def largest_prime_factor(number) 
   
 end
